ae8b697573d8ce11a992652929caed8d0c9baceb
Yixun Tan <yixun_tan@berkeley.edu>
2019-05-04 18:04:40 +0000

    Add results from the optimized models (Automated tuning).

--- XGB_Automated_Tuning.py
+++ XGB_Automated_Tuning.py
@@ -16,13 +16,9 @@ import pickle
 import warnings
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv('Feature/final_dataset_train.csv')
+df = pd.read_csv('/home/Ethan_google/final_dataset_train.csv')
 y_train = df['TARGET']
 x_train = df.drop(['TARGET', 'SK_ID_CURR'], axis = 1)
-
-clr_xgb = xgb.XGBClassifier(n_estimators=500, n_jobs = -1)
-roc_auc_scores = cross_val_score(clr_xgb, x_train, y_train, cv=5, scoring='roc_auc')
-print('5 fold CV ROC_AUC is %0.2f (+/- %0.2f)' %(roc_auc_scores.mean(), roc_auc_scores.std() * 2))
 
 import csv
 from hyperopt import STATUS_OK
@@ -35,7 +31,8 @@ def objective(hyperparameters):
     ITERATION += 1
         
     start = timer()
-    clf = xgb.XGBClassifier(**hyperparameters, n_jobs = -1)
+    
+    clf = xgb.XGBClassifier(**hyperparameters, n_jobs = -1, n_estimators = 500)
     # Perform n_fold cross validation
     roc_auc_scores = cross_val_score(clf, x_train, y_train, cv = N_FOLDS, scoring = 'roc_auc')
     
@@ -94,11 +91,11 @@ global  ITERATION
 ITERATION = 0
 # Governing choices for search
 N_FOLDS = 5
-MAX_EVALS = 100
+MAX_EVALS = 500
 
 best = fmin(fn = objective, space = space, algo = tpe.suggest, trials = trials,
             max_evals = MAX_EVALS)
 
 
-with open('Trial_XGB_100.pkl', 'wb') as file:
+with open('Trial_XGB_500.pkl', 'wb') as file:
 	pickle.dump(trials, file)
